8ba9643cb845b7e7c7de3fadefc1881485cf21a1
shchnk <761962425@qq.com>
2023-06-28 15:40:29 +0800

    bug fix

---
+++ src/app/api/articles/by-tag/route.js
@@ -0,0 +1,16 @@
+import Test from "@/models/articles";
+import { connectToDatabase } from "@/utils/database";
+
+export const GET = async (req, res) => {
+  try {
+    await connectToDatabase();
+
+    const articles = await Test.find({});
+
+    return new Response(JSON.stringify(articles), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify("Failed to fetch a new article"), {
+      status: 500,
+    });
+  }
+};
